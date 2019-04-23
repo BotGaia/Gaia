71faa4bcc0eb9da494cc8a91cebfe23a88aefa8b
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-04-23 09:37:20 -0300

    Stable version

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -2,14 +2,23 @@ from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
 import requests
 import random
+import json
 
-class ActionTest(Action):
-   def name(self):
-      return "action_test"
 
-   def run(self, dispatcher, tracker, domain):
+class Action_test(Action):
+    def name(self):
+        return "action_test"
+
+    def run(self, dispatcher, tracker, domain):
+        locale = tracker.get_slot('locale')
+        payload = {'place': 'locale'}
+        response = requests.get(
+            'http://68.183.43.29:30000/request', params=payload)
+        answer = response.json()
+        print('/n/n/n/n/n')
+        print(answer)
+        print('/n/n/n/n/n')
         try:
-          dispatcher.utter_message("Mensagem enviada por uma custom action.")
+            dispatcher.utter_message(answer["sky"])
         except ValueError:
-          dispatcher.utter_message(ValueError)
-
+            dispatcher.utter_message(ValueError)
