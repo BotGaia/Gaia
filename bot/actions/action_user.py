e6abd9c56e0e673f6b4fbef616123edf817f56cf
Micaella <micaella2212@gmail.com>
2019-05-11 00:20:15 -0300

    Adding new type of slots to array
    Co-authored-by: Sofia Patrocinio <sofiapatrocinio@hotmail.com>
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- bot/actions/action_user.py
+++ bot/actions/action_user.py
@@ -12,8 +12,8 @@ class User_Action(Action):
     def run(self, dispatcher, tracker, domain):
         tracker_state = tracker.current_state()
         sender_id = tracker_state['sender_id']
-        user_local = tracker.get_slot('locale')
-        user_sport = tracker.get_slot('sport')
+        user_local = tracker.get_slot('user_locale')
+        user_sport = tracker.get_slot('user_sport')
         user_day = tracker.get_slot('user_day')
         user_hour = tracker.get_slot('user_hour')
         user_minute = tracker.get.slot('user_minute')
@@ -26,7 +26,7 @@ class User_Action(Action):
             "times": [ { user_hour, user_minute }],
             "local": [user_local]
             }
+        dispatcher.utter_message(sender_id)
+        dispatcher.utter_message(user_local)
 
         response = requests.post('http://'+ip_address+':3003/createNotification', data = dataJson)
-
-
