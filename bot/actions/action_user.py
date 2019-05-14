d6fb15a99eebd402e346d79d781a5183f68877ed
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2019-05-14 12:02:28 -0300

    Fixing slot type list
    Co-authored-by: Eduardo Lima <eduardolima.df@gmail.com>
    Co-authored-by: Luis Taira <luishptaira@gmail.com>

--- bot/actions/action_user.py
+++ bot/actions/action_user.py
@@ -1,5 +1,6 @@
 from rasa_core_sdk import Action
 from rasa_core_sdk.events import SlotSet
+from typing import List
 import requests
 import random
 import json
@@ -13,8 +14,11 @@ class User_Action(Action):
         tracker_state = tracker.current_state()
         sender_id = tracker_state['sender_id']
         user_local = tracker.get_slot('user_locale')
+        local_user = ''.join(user_local)
         user_sport = tracker.get_slot('user_sport')
+        sport_user = ''.join(user_sport)
         user_day = tracker.get_slot('user_day')
+        day_user = ''.join(user_day)
         user_hour = tracker.get_slot('user_hour')
         user_minute = tracker.get_slot('user_minute')
 
@@ -25,7 +29,12 @@ class User_Action(Action):
         #     "times": [ { user_hour, user_minute }],
         #     "local": [user_local]
         #     }
-        dispatcher.utter_message(sender_id)
-        dispatcher.utter_message(user_local)
+        # dispatcher.utter_message(sender_id)
+        dispatcher.utter_message(local_user)
+        dispatcher.utter_message(sport_user)
+        dispatcher.utter_message(day_user)
+        # dispatcher.utter_message(user_hour)
+        # dispatcher.utter_message(user_minute)
+
 
         # response = requests.post('http://'+ip_address+':3003/createNotification', data = dataJson)
