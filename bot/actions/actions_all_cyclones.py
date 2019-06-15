e82f8621c15a0fd0c4ed78c2954d198035de8185
Eduardo <eduardolima.df@gmail.com>
2019-06-14 23:55:27 -0300

    Solving flake8

--- bot/actions/actions_all_cyclones.py
+++ bot/actions/actions_all_cyclones.py
@@ -13,23 +13,28 @@ class All_cyclones_Action(Action):
         payload = {
           "intent": "allCyclones"
         }
-        response = requests.get(URL + 'ciclone',params = payload)
+        response = requests.get(URL + 'ciclone', params=payload)
         answer = response.content.decode()
         answer_json = json.loads(answer)
         counter = 1
 
         if(len(answer_json) > 0):
-         for cyclone in answer_json:
-            data_message = str(counter) + '. ' + cyclone["name"] + '\n'
-            data_message += 'Lugar de origem: ' + cyclone["originBasin"] + '\n'
-            data_message += 'Ocorrência: ' + cyclone["currentBasin"] + '\n'
-            data_message += 'Data de início: ' + cyclone["startDate"] + '\n'
-            data_message += 'Tipo de fenômeno: ' + cyclone["stormType"] + '\n'
-            data_message += 'Velocidade do vento: ' + str(cyclone["windSpeed"]) + '\n'
-            dispatcher.utter_message(data_message)
-            counter += 1
+            for cyclone in answer_json:
+                windspd = 'Velocidade do vento: '
+                origin = 'Lugar de origem: '
+                ocurrency = 'Ocorrência: '
+                startDate = 'Data de início: '
+                stormtype = 'Tipo de fenômeno: '
+                data_message = str(counter) + '. ' + cyclone["name"] + '\n'
+                data_message += origin + cyclone["originBasin"] + '\n'
+                data_message += ocurrency + cyclone["currentBasin"] + '\n'
+                data_message += startDate + cyclone["startDate"] + '\n'
+                data_message += stormtype + cyclone["stormType"] + '\n'
+                data_message += windspd + str(cyclone["windSpeed"]) + '\n'
+                dispatcher.utter_message(data_message)
+                counter += 1
 
         else:
             message1 = 'Não houve ocorrência de '
-            message2= 'ciclone nas últmas duas horas'
+            message2 = 'ciclone nas últmas duas horas'
             dispatcher.utter_message(message1 + message2)
